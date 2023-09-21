1eac8182ef602f0bbaad577715591e7d670bf6ad
JunHaoShih <victor5517@gmail.com>
2023-09-21 15:41:53 +0800

    feat: prevent diagram from text select

--- src/modules/diagrams/components/EntitySvg.vue
+++ src/modules/diagrams/components/EntitySvg.vue
@@ -1,6 +1,9 @@
+<!-- eslint-disable max-len -->
 <template>
-  <g class="prevent-select"
-      v-touch-pan.prevent.mouse="handleDrag">
+  <g
+    class="prevent-select"
+    v-touch-pan.prevent.mouse="handleDrag"
+  >
     // drag area
     <path
       :d="dragPath"
@@ -51,6 +54,21 @@
       v-bind:key="row.name"
       @dblclick="row.isEditing = true"
     >
+      // primary key
+      <svg
+        v-if="row.isPrimary"
+        :x="point.x"
+        :y="getRowStartY(index) - 26"
+        height="20"
+        viewBox="0 -960 960 960"
+        width="20"
+      >
+        // key svg icon
+        <path
+          d="M280-400q-33 0-56.5-23.5T200-480q0-33 23.5-56.5T280-560q33 0 56.5 23.5T360-480q0 33-23.5 56.5T280-400Zm0 160q-100 0-170-70T40-480q0-100 70-170t170-70q67 0 121.5 33t86.5 87h352l120 120-180 180-80-60-80 60-85-60h-47q-32 54-86.5 87T280-240Zm0-80q56 0 98.5-34t56.5-86h125l58 41 82-61 71 55 75-75-40-40H435q-14-52-56.5-86T280-640q-66 0-113 47t-47 113q0 66 47 113t113 47Z"
+          fill="coral"
+        />
+      </svg>
       <path
         :d="getRowBottomPath(index)"
         stroke="black"
@@ -232,13 +250,13 @@ const rows = ref<TableRow[]>([
   },
   {
     name: 'fff',
-    isPrimary: true,
+    isPrimary: false,
     type: 'int',
     uniqueKeys: [],
   },
   {
     name: 'ggg',
-    isPrimary: true,
+    isPrimary: false,
     type: 'int',
     uniqueKeys: [],
   },
@@ -335,9 +353,4 @@ onBeforeMount(() => {
 </script>
 
 <style lang="scss" scoped>
-.prevent-select {
-  -webkit-user-select: none; /* Safari */
-  -ms-user-select: none; /* IE 10 and IE 11 */
-  user-select: none; /* Standard syntax */
-}
 </style>
