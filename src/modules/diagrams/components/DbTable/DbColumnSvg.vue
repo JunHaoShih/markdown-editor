3772cac533b8aeb2759a5ad2df54c56bc65b7d80
JunHaoShih <victor5517@gmail.com>
2023-09-22 16:14:08 +0800

    feat: add boolean svg

--- src/modules/diagrams/components/DbTable/DbColumnSvg.vue
+++ src/modules/diagrams/components/DbTable/DbColumnSvg.vue
@@ -1,23 +1,33 @@
 <template>
   // primary key
-  <svg
-    v-if="dbColumn.isPrimary"
+  <BooleanSvg
+    v-model="dbColumn.isPrimary"
     :x="x + 2"
     :y="y + 3"
-    height="20"
-    viewBox="0 -960 960 960"
-    width="20"
+    :height="dbColumn.height"
+    :width="iconWidth"
   >
-    // key svg icon
-    <path
-      d="M280-400q-33 0-56.5-23.5T200-480q0-33 23.5-56.5T280-560q33 0 56.5 23.5T360-480q0
-      33-23.5 56.5T280-400Zm0 160q-100 0-170-70T40-480q0-100 70-170t170-70q67 0 121.5
-      33t86.5 87h352l120 120-180 180-80-60-80 60-85-60h-47q-32 54-86.5 87T280-240Zm0-80q56 0
-      98.5-34t56.5-86h125l58 41 82-61 71 55 75-75-40-40H435q-14-52-56.5-86T280-640q-66 0-113
-      47t-47 113q0 66 47 113t113 47Z"
-      fill="coral"
-    />
-  </svg>
+    <template v-slot:display="props">
+      <svg
+        v-if="props.isTrue"
+        :x="x + 2"
+        :y="y + 3"
+        height="20"
+        viewBox="0 -960 960 960"
+        width="20"
+      >
+        // key svg icon
+        <path
+          d="M280-400q-33 0-56.5-23.5T200-480q0-33 23.5-56.5T280-560q33 0 56.5 23.5T360-480q0
+          33-23.5 56.5T280-400Zm0 160q-100 0-170-70T40-480q0-100 70-170t170-70q67 0 121.5
+          33t86.5 87h352l120 120-180 180-80-60-80 60-85-60h-47q-32 54-86.5 87T280-240Zm0-80q56 0
+          98.5-34t56.5-86h125l58 41 82-61 71 55 75-75-40-40H435q-14-52-56.5-86T280-640q-66 0-113
+          47t-47 113q0 66 47 113t113 47Z"
+          fill="coral"
+        />
+      </svg>
+    </template>
+  </BooleanSvg>
   // bottom line
   <path
     :d="getRowBottomPath()"
@@ -94,6 +104,7 @@ import { computed } from 'vue';
 import { DbTableColumn } from './DbTable';
 import RightResizeLine from './RightResizeLine.vue';
 import TextSvg from '../TextSvg.vue';
+import BooleanSvg from '../BooleanSvg.vue';
 
 const minIconWidth = 30;
 
