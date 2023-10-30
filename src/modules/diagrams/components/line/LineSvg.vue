7ca436b010112fb6f7ab48e79365ed9e0f8bf9c3
JunHaoShih <victor5517@gmail.com>
2023-10-30 11:51:04 +0800

    feat: add diagnal line

--- src/modules/diagrams/components/line/LineSvg.vue
+++ src/modules/diagrams/components/line/LineSvg.vue
@@ -23,6 +23,7 @@
         />
       </marker>
     </defs>
+    <!--
     <line
       :x1="fromPoint.x"
       :y1="fromPoint.y"
@@ -39,6 +40,19 @@
       style="stroke:rgb(0, 0, 0);stroke-width:1"
       :marker-start="line.arrowStart ? `url(#${line.arrowStart})` : ''"
       :marker-end="line.arrowEnd ? `url(#${line.arrowEnd})` : ''"
+    />-->
+    <path
+      :d="path"
+      fill="none"
+      stroke-width="5"
+      :stroke="isSelected ? 'rgb(10, 213, 240)' : 'transparent'"
+    />
+    <path
+      :d="path"
+      style="stroke:rgb(0, 0, 0);stroke-width:1"
+      fill="none"
+      :marker-start="line.arrowStart ? `url(#${line.arrowStart})` : ''"
+      :marker-end="line.arrowEnd ? `url(#${line.arrowEnd})` : ''"
     />
   </ShapeSlot>
 </template>
@@ -49,8 +63,12 @@ import {
 } from 'vue';
 import { markers } from './line';
 import ShapeSlot from '../ShapeSlot.vue';
-import { Shape } from '../../models/shape';
+import { Point, Shape } from '../../models/shape';
 import { useDiagramStore } from '../../stores/diagramStore';
+
+const startDistance = ref(30);
+
+const endDistance = ref(30);
 
 const isSelected = ref(false);
 
@@ -109,6 +127,96 @@ watch(toNode, (newValue) => {
 }, {
   deep: true,
 });
+
+/**
+ * Parse angle radian to degree
+ * @param radians The radian you want to parse
+ */
+function toDegrees(radians: number) {
+  return radians * (180 / Math.PI);
+}
+
+/**
+ * Parse angle degree to radian
+ * @param degrees The degree you want to parse
+ */
+function toRadians(degrees: number) {
+  return degrees * (Math.PI / 180);
+}
+
+/**
+ * Normalize 2d vector
+ * @param vector The 2d vector you want to normalize
+ */
+function normalize2dVector(vector: number[]) {
+  const magnitude = Math.sqrt((vector[0] * vector[0]) + (vector[1] * vector[1]));
+  return vector.map((length) => length / magnitude);
+}
+
+/**
+ * Rotate 2d vector with rotation matrix
+ * @param vector target vector
+ * @param radians rotate radians
+ */
+function rotate2dVector(vector: number[], radians: number) {
+  const newVector: number[] = [];
+  const x = vector[0] * Math.cos(radians) - vector[1] * Math.sin(radians);
+  const y = vector[0] * Math.sin(radians) + vector[1] * Math.cos(radians);
+
+  newVector.push(x, y);
+  return newVector;
+}
+
+function findDegrees(from: Point, to: Point) {
+  const dot = (from.x * to.x) + (from.y * to.y);
+  const det = (from.x * to.y) - (from.y * to.x);
+  const angle = Math.atan2(det, dot);
+  const currentDegrees = toDegrees(angle);
+  return Math.floor((currentDegrees + 45) / 90) * 90;
+}
+
+const startPoint = computed(
+  (): Point => {
+    const degrees = fromNode.value
+      ? fromNode.value.orient
+      : findDegrees({
+        x: 0, y: -1,
+      }, {
+        x: toPoint.value.x - fromPoint.value.x,
+        y: toPoint.value.y - fromPoint.value.y,
+      });
+    const rotatedVector = rotate2dVector([0, -startDistance.value], toRadians(degrees));
+    return {
+      x: fromPoint.value.x + rotatedVector[0],
+      y: fromPoint.value.y + rotatedVector[1],
+    };
+  },
+);
+
+const endPoint = computed(
+  (): Point => {
+    const degrees = toNode.value
+      ? toNode.value.orient
+      : findDegrees({
+        x: 0, y: -1,
+      }, {
+        x: fromPoint.value.x - toPoint.value.x,
+        y: fromPoint.value.y - toPoint.value.y,
+      });
+    const rotatedVector = rotate2dVector([0, -endDistance.value], toRadians(degrees));
+    return {
+      x: toPoint.value.x + rotatedVector[0],
+      y: toPoint.value.y + rotatedVector[1],
+    };
+  },
+);
+
+const path = computed(
+  () => `M ${fromPoint.value.x} ${fromPoint.value.y} \
+  L ${startPoint.value.x} ${startPoint.value.y} \
+  L ${endPoint.value.x} ${endPoint.value.y} \
+  L ${toPoint.value.x} ${toPoint.value.y}`,
+);
 
 onBeforeMount(() => {
   if (toNode.value) {
