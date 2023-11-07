67d878e682e5b85dcf949239b51a274615a355c6
JunHaoShih <victor5517@gmail.com>
2023-11-07 09:32:03 +0800

    feat: add target blank to all links

--- src/modules/markdown/components/MarkdownViewer.vue
+++ src/modules/markdown/components/MarkdownViewer.vue
@@ -45,6 +45,23 @@ const md: MarkdownIt = new MarkdownIt({
   },
 });
 
+const defaultLinkRender = md.renderer.rules.link_open
+  || ((tokens, idx, options, env, self) => self.renderToken(tokens, idx, options));
+
+md.renderer.rules.link_open = ((tokens, idx, options, env, self) => {
+  // If you are sure other plugins can't add `target` - drop check below
+  const aIndex = tokens[idx].attrIndex('target');
+
+  if (aIndex < 0) {
+    tokens[idx].attrPush(['target', '_blank']); // add new attribute
+  } else {
+    tokens[idx].attrSet('target', '_blank');// replace value of existing attr
+  }
+
+  // pass token to default renderer.
+  return defaultLinkRender(tokens, idx, options, env, self);
+});
+
 const markdown = computed(
   () => md.render(mdText.value),
 );
