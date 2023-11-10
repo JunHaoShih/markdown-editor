20acaf15f6bc7a9419130e03d76044fab90e1aa2
JunHaoShih <victor5517@gmail.com>
2023-11-10 10:36:54 +0800

    refactor: change store to composition api

--- src/modules/diagrams/stores/diagramStore.ts
+++ src/modules/diagrams/stores/diagramStore.ts
@@ -1,4 +1,5 @@
 import { defineStore } from 'pinia';
+import { computed, ref } from 'vue';
 import { ConnectionNode, Diagram, Point } from '../models/shape';
 
 export type HoldType = 'none' | 'connect' | 'reconnect' | 'multiSelect' | 'drag';
@@ -23,75 +24,68 @@ export interface SelectStructure {
   children: SelectStructure[],
 }
 
-interface DiagramStore {
-  diagram: Diagram,
-  offsetX: number,
-  offsetY: number,
-  holdInfo: MouseHoldInfo,
-  selectedIds: SelectStructure[],
-  dragShift: Point,
-}
+export const useDiagramStore = defineStore('diagram', () => {
+  const diagram = ref<Diagram>({
+    shapes: [],
+  });
 
-interface Rectangle {
-  x: number,
-  y: number,
-  width: number,
-  height: number,
-}
+  const offsetX = ref(0);
 
-export const useDiagramStore = defineStore('diagram', {
-  state: (): DiagramStore => ({
-    diagram: {
-      shapes: [],
-    },
-    offsetX: 0,
-    offsetY: 0,
-    holdInfo: {
-      type: 'none',
-      isHold: false,
-      movedNode: 'none',
-      holdFrom: {
-        x: 0,
-        y: 0,
-      },
-      holdTo: {
-        x: 0,
-        y: 0,
-      },
-    },
-    selectedIds: [],
-    dragShift: {
+  const offsetY = ref(0);
+
+  const holdInfo = ref<MouseHoldInfo>({
+    type: 'none',
+    isHold: false,
+    movedNode: 'none',
+    holdFrom: {
       x: 0,
       y: 0,
     },
-  }),
-  getters: {
-    holdType: (state) => state.holdInfo.type,
-    pointShift: (state) => (point: Point): Point => {
-      const shiftedPoint: Point = {
-        x: point.x - state.offsetX,
-        y: point.y - state.offsetY,
-      };
-      return shiftedPoint;
+    holdTo: {
+      x: 0,
+      y: 0,
     },
-    holdFrom: (state) => state.holdInfo.holdFrom,
-    holdTo: (state) => state.holdInfo.holdTo,
-    connectionNode: (state) => (nodeId: string): ConnectionNode | undefined => {
-      const targetNode = state.diagram.shapes
-        .flatMap((shape) => {
-          const nodes: ConnectionNode[] = [];
-          nodes.push(...shape.connectionNodes);
-          if (shape.dbColumns) {
-            nodes.push(...shape.dbColumns.flatMap((col) => col.connectionNodes));
-          }
-          return nodes;
-        })
-        .find((node) => node.id === nodeId);
-      return targetNode;
-    },
-    selectedSquare: (state): Rectangle => {
-      const targetShapes = state.diagram.shapes
-        .filter((shape) => state.selectedIds.map((selected) => selected.id).includes(shape.id));
+  });
+
+  const selectedIds = ref<SelectStructure[]>([]);
+
+  const dragShift = ref<Point>({
+    x: 0,
+    y: 0,
+  });
+
+  const holdType = computed(() => holdInfo.value.type);
+
+  function pointShift(point: Point): Point {
+    const shiftedPoint: Point = {
+      x: point.x - offsetX.value,
+      y: point.y - offsetY.value,
+    };
+    return shiftedPoint;
+  }
+
+  const holdFrom = computed(() => holdInfo.value.holdFrom);
+
+  const holdTo = computed(() => holdInfo.value.holdTo);
+
+  function connectionNode(nodeId: string): ConnectionNode | undefined {
+    const targetNode = diagram.value.shapes
+      .flatMap((shape) => {
+        const nodes: ConnectionNode[] = [];
+        nodes.push(...shape.connectionNodes);
+        if (shape.dbColumns) {
+          nodes.push(...shape.dbColumns.flatMap((col) => col.connectionNodes));
+        }
+        return nodes;
+      })
+      .find((node) => node.id === nodeId);
+    return targetNode;
+  }
+
+  const selectedSquare = computed(
+    () => {
+      const targetShapes = diagram.value.shapes
+        .filter((shape) => selectedIds.value.map((selected) => selected.id).includes(shape.id));
       const minX = Math.min(...targetShapes.map((shape) => shape.position.x));
       const minY = Math.min(...targetShapes.map((shape) => shape.position.y));
       const maxX = Math.max(
@@ -101,202 +95,229 @@ export const useDiagramStore = defineStore('diagram', {
         ...targetShapes.map((shape) => shape.position.y + (shape.height ?? 0)),
       );
       return {
-        x: minX + state.dragShift.x,
-        y: minY + state.dragShift.y,
+        x: minX + dragShift.value.x,
+        y: minY + dragShift.value.y,
         width: maxX - minX,
         height: maxY - minY,
       };
     },
-  },
-  actions: {
-    setBoudings(dom: DOMRect) {
-      this.offsetX = dom.left;
-      this.offsetY = dom.top;
+  );
+
+  function setBoudings(dom: DOMRect) {
+    offsetX.value = dom.left;
+    offsetY.value = dom.top;
+  }
+
+  /**
+   * Start connect new line
+   * @param x Starting x coordinate
+   * @param y Starting y coordinate
+   * @param fromId from which node
+   * @param shapeId from which shape
+   */
+  function startConnect(x: number, y: number, fromId: string, shapeId: string) {
+    holdInfo.value.holdFrom.x = x;
+    holdInfo.value.holdFrom.y = y;
+    holdInfo.value.holdFromShape = shapeId;
+    holdInfo.value.type = 'connect';
+    holdInfo.value.movedNode = 'to';
+    holdInfo.value.holdToId = '';
+    holdInfo.value.holdLineId = '';
+    holdInfo.value.holdToShape = '';
+    selectedIds.value.length = 0;
+    holdInfo.value.holdFromId = fromId;
+  }
+
+  function startReconnectFrom(
+    movedType: MovedType,
+    lineId: string,
+    from: Point,
+    to: Point,
+    fromId?: string,
+    toId?: string,
+    fromShape?: string,
+    toShape?: string,
+  ) {
+    holdInfo.value.movedNode = movedType;
+    holdInfo.value.holdLineId = lineId;
+
+    holdInfo.value.holdFrom.x = from.x;
+    holdInfo.value.holdFrom.y = from.y;
+
+    holdInfo.value.holdTo.x = to.x;
+    holdInfo.value.holdTo.y = to.y;
+
+    holdInfo.value.holdFromId = fromId;
+    holdInfo.value.holdToId = toId;
+    holdInfo.value.holdFromShape = fromShape;
+    holdInfo.value.holdToShape = toShape;
+    holdInfo.value.type = 'reconnect';
+    selectedIds.value.length = 0;
+  }
+
+  function startHolding(type: HoldType, x: number, y: number) {
+    holdInfo.value.holdFrom.x = x;
+    holdInfo.value.holdFrom.y = y;
+    holdInfo.value.holdTo.x = x;
+    holdInfo.value.holdTo.y = y;
+    holdInfo.value.type = type;
+    holdInfo.value.holdToId = '';
+    holdInfo.value.holdLineId = '';
+  }
+
+  function movingByEvent(evt: MouseEvent) {
+    if (holdInfo.value.type === 'reconnect' && holdInfo.value.movedNode === 'from') {
+      holdInfo.value.holdFrom.x = evt.x - offsetX.value;
+      holdInfo.value.holdFrom.y = evt.y - offsetY.value;
+    } else {
+      holdInfo.value.holdTo.x = evt.x - offsetX.value;
+      holdInfo.value.holdTo.y = evt.y - offsetY.value;
+    }
+  }
+
+  function endHolding() {
+    holdInfo.value.type = 'none';
+    holdInfo.value.holdFromShape = '';
+    holdInfo.value.holdFromId = '';
+    holdInfo.value.holdToShape = '';
+    holdInfo.value.holdToId = '';
+    holdInfo.value.holdLineId = '';
+    holdInfo.value.movedNode = 'none';
+  }
+
+  function setConnectionNodeId(nodeId: string, shapeId: string) {
+    if (holdInfo.value.movedNode === 'from') {
+      holdInfo.value.holdFromId = nodeId;
+      holdInfo.value.holdFromShape = shapeId;
+    } else if (holdInfo.value.movedNode === 'to') {
+      holdInfo.value.holdToId = nodeId;
+      holdInfo.value.holdToShape = shapeId;
+    }
+  }
+
+  function unsetConnectionNodeId(nodeId: string) {
+    if (holdInfo.value.movedNode === 'from' && holdInfo.value.holdFromId === nodeId) {
+      holdInfo.value.holdFromId = undefined;
+      holdInfo.value.holdFromShape = undefined;
+    } else if (holdInfo.value.movedNode === 'to' && holdInfo.value.holdToId === nodeId) {
+      holdInfo.value.holdToId = undefined;
+      holdInfo.value.holdToShape = undefined;
+    }
+  }
+
+  function getHoldedLine() {
+    const index = diagram.value.shapes.findIndex((shape) => shape.id === holdInfo.value.holdLineId);
+    if (index === -1) {
+      return undefined;
+    }
+    return diagram.value.shapes[index];
+  }
+
+  /**
+   * Delete shape by id
+   * @param id Shape id
+   */
+  function deleteShape(id: string) {
+    const index = diagram.value.shapes.findIndex((shape) => shape.id === id);
+    if (index === -1) {
+      return;
+    }
+    diagram.value.shapes.splice(index, 1);
+  }
+
+  /**
+   * Add id to selected list
+   * @param id Shape id what is selected
+   */
+  function addSelectedId(id: string) {
+    const target = selectedIds.value.find((selected) => selected.id === id);
+    if (!target) {
+      selectedIds.value.push({
+        id,
+        children: [],
+      });
+    }
+  }
+
+  /**
+   * Set id to selected list
+   * @param id Shape id what is selected
+   */
+  function setSelectedId(id: string) {
+    const target = selectedIds.value.find((selected) => selected.id === id);
+    if (!target) {
+      selectedIds.value.push({
+        id,
+        children: [],
+      });
+    } else {
+      selectedIds.value[0] = target;
+      selectedIds.value.length = 1;
+    }
+  }
+
+  function handleDrag(details: {
+    isFirst?: boolean,
+    isFinal?: boolean,
+    delta?: {
+      x?: number,
+      y?: number,
     },
+  }) {
+    if (details.isFirst) {
+      dragShift.value.x = 0;
+      dragShift.value.y = 0;
+      holdInfo.value.type = 'drag';
+      return;
+    }
+    if (details.isFinal) {
+      selectedIds.value.forEach((selected) => {
+        const targetShape = diagram.value.shapes.find((shape) => shape.id === selected.id);
+        if (!targetShape) {
+          return;
+        }
+        if (targetShape.type === 'line') {
+          return;
+        }
+        targetShape.position.x += dragShift.value.x;
+        targetShape.position.y += dragShift.value.y;
+      });
+      holdInfo.value.type = 'none';
+      return;
+    }
+    if (details.delta?.x) {
+      dragShift.value.x += details.delta.x;
+    }
+    if (details.delta?.y) {
+      dragShift.value.y += details.delta.y;
+    }
+  }
 
-    /**
-     * Start connect new line
-     * @param x Starting x coordinate
-     * @param y Starting y coordinate
-     * @param fromId from which node
-     * @param shapeId from which shape
-     */
-    startConnect(x: number, y: number, fromId: string, shapeId: string) {
-      this.holdInfo.holdFrom.x = x;
-      this.holdInfo.holdFrom.y = y;
-      this.holdInfo.holdFromShape = shapeId;
-      this.holdInfo.type = 'connect';
-      this.holdInfo.movedNode = 'to';
-      this.holdInfo.holdToId = '';
-      this.holdInfo.holdLineId = '';
-      this.holdInfo.holdToShape = '';
-      this.selectedIds.length = 0;
-      this.holdInfo.holdFromId = fromId;
-    },
-
-    startReconnectFrom(
-      movedType: MovedType,
-      lineId: string,
-      from: Point,
-      to: Point,
-      fromId?: string,
-      toId?: string,
-      fromShape?: string,
-      toShape?: string,
-    ) {
-      this.holdInfo.movedNode = movedType;
-      this.holdInfo.holdLineId = lineId;
-
-      this.holdInfo.holdFrom.x = from.x;
-      this.holdInfo.holdFrom.y = from.y;
-
-      this.holdInfo.holdTo.x = to.x;
-      this.holdInfo.holdTo.y = to.y;
-
-      this.holdInfo.holdFromId = fromId;
-      this.holdInfo.holdToId = toId;
-      this.holdInfo.holdFromShape = fromShape;
-      this.holdInfo.holdToShape = toShape;
-      this.holdInfo.type = 'reconnect';
-      this.selectedIds.length = 0;
-    },
-
-    startHolding(type: HoldType, x: number, y: number) {
-      this.holdInfo.holdFrom.x = x;
-      this.holdInfo.holdFrom.y = y;
-      this.holdInfo.holdTo.x = x;
-      this.holdInfo.holdTo.y = y;
-      this.holdInfo.type = type;
-      this.holdInfo.holdToId = '';
-      this.holdInfo.holdLineId = '';
-    },
-
-    movingByEvent(evt: MouseEvent) {
-      if (this.holdInfo.type === 'reconnect' && this.holdInfo.movedNode === 'from') {
-        this.holdInfo.holdFrom.x = evt.x - this.offsetX;
-        this.holdInfo.holdFrom.y = evt.y - this.offsetY;
-      } else {
-        this.holdInfo.holdTo.x = evt.x - this.offsetX;
-        this.holdInfo.holdTo.y = evt.y - this.offsetY;
-      }
-    },
-
-    endHolding() {
-      this.holdInfo.type = 'none';
-      this.holdInfo.holdFromShape = '';
-      this.holdInfo.holdFromId = '';
-      this.holdInfo.holdToShape = '';
-      this.holdInfo.holdToId = '';
-      this.holdInfo.holdLineId = '';
-      this.holdInfo.movedNode = 'none';
-    },
-
-    setConnectionNodeId(nodeId: string, shapeId: string) {
-      if (this.holdInfo.movedNode === 'from') {
-        this.holdInfo.holdFromId = nodeId;
-        this.holdInfo.holdFromShape = shapeId;
-      } else if (this.holdInfo.movedNode === 'to') {
-        this.holdInfo.holdToId = nodeId;
-        this.holdInfo.holdToShape = shapeId;
-      }
-    },
-
-    unsetConnectionNodeId(nodeId: string) {
-      if (this.holdInfo.movedNode === 'from' && this.holdInfo.holdFromId === nodeId) {
-        this.holdInfo.holdFromId = undefined;
-        this.holdInfo.holdFromShape = undefined;
-      } else if (this.holdInfo.movedNode === 'to' && this.holdInfo.holdToId === nodeId) {
-        this.holdInfo.holdToId = undefined;
-        this.holdInfo.holdToShape = undefined;
-      }
-    },
-
-    getHoldedLine() {
-      const index = this.diagram.shapes.findIndex((shape) => shape.id === this.holdInfo.holdLineId);
-      if (index === -1) {
-        return undefined;
-      }
-      return this.diagram.shapes[index];
-    },
-
-    /**
-     * Delete shape by id
-     * @param id Shape id
-     */
-    deleteShape(id: string) {
-      const index = this.diagram.shapes.findIndex((shape) => shape.id === id);
-      if (index === -1) {
-        return;
-      }
-      this.diagram.shapes.splice(index, 1);
-    },
-
-    /**
-     * Add id to selected list
-     * @param id Shape id what is selected
-     */
-    addSelectedId(id: string) {
-      const target = this.selectedIds.find((selected) => selected.id === id);
-      if (!target) {
-        this.selectedIds.push({
-          id,
-          children: [],
-        });
-      }
-    },
-
-    /**
-     * Set id to selected list
-     * @param id Shape id what is selected
-     */
-    setSelectedId(id: string) {
-      const target = this.selectedIds.find((selected) => selected.id === id);
-      if (!target) {
-        this.selectedIds.push({
-          id,
-          children: [],
-        });
-      } else {
-        this.selectedIds[0] = target;
-        this.selectedIds.length = 1;
-      }
-    },
-
-    handleDrag(details: {
-      isFirst?: boolean,
-      isFinal?: boolean,
-      delta?: {
-        x?: number,
-        y?: number,
-      },
-    }) {
-      if (details.isFirst) {
-        this.dragShift.x = 0;
-        this.dragShift.y = 0;
-        this.holdInfo.type = 'drag';
-        return;
-      }
-      if (details.isFinal) {
-        this.selectedIds.forEach((selected) => {
-          const targetShape = this.diagram.shapes.find((shape) => shape.id === selected.id);
-          if (!targetShape) {
-            return;
-          }
-          if (targetShape.type === 'line') {
-            return;
-          }
-          targetShape.position.x += this.dragShift.x;
-          targetShape.position.y += this.dragShift.y;
-        });
-        this.holdInfo.type = 'none';
-        return;
-      }
-      if (details.delta?.x) {
-        this.dragShift.x += details.delta.x;
-      }
-      if (details.delta?.y) {
-        this.dragShift.y += details.delta.y;
-      }
-    },
-  },
+  return {
+    diagram,
+    offsetX,
+    offsetY,
+    holdInfo,
+    selectedIds,
+    dragShift,
+    holdType,
+    pointShift,
+    holdFrom,
+    holdTo,
+    connectionNode,
+    selectedSquare,
+    setBoudings,
+    startConnect,
+    startReconnectFrom,
+    startHolding,
+    movingByEvent,
+    endHolding,
+    setConnectionNodeId,
+    unsetConnectionNodeId,
+    getHoldedLine,
+    deleteShape,
+    addSelectedId,
+    setSelectedId,
+    handleDrag,
+  };
 });
