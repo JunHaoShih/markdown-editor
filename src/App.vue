c3f47c673ff12d881a07bf5599f0b57c0a0a8507
JunHaoShih <victor5517@gmail.com>
2023-11-16 17:22:03 +0800

    feat: add dark class on body

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,26 @@
 <template>
-  <router-view />
+  <div class="tw-bg-gray-50 dark:tw-bg-darkBg">
+    <router-view />
+  </div>
 </template>
 
 <script setup lang="ts">
+import { useDarkStore } from 'src/stores/darkModeStore';
+import { watch } from 'vue';
 
+const darkStore = useDarkStore();
+
+watch(() => darkStore.isDark, (isDark) => {
+  const body = document.querySelector('#root-body');
+  if (!body) {
+    return;
+  }
+  if (isDark && !body.classList.contains('tw-dark')) {
+    body.classList.add('tw-dark');
+  } else if (!isDark && body.classList.contains('tw-dark')) {
+    body.classList.remove('tw-dark');
+  }
+}, {
+  immediate: true,
+});
 </script>
