a42ff41ed85b0789155f82fca5ec7e23e80c1a8f
JunHaoShih <victor5517@gmail.com>
2023-11-21 13:39:22 +0800

    feat: add workspace index

---
+++ src/pages/workspace/IndexPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="tw-flex tw-flex-col tw-justify-center tw-items-center tw-px-6
+    tw-mx-auto tw-h-screen xl:tw-px-0 dark:tw-bg-stone-800"
+  >
+    <div class="tw-text-center xl:tw-max-w-4xl">
+      <h1 class="tw-mb-3 tw-text-2xl tw-font-bold tw-leading-tight tw-text-gray-900
+        sm:tw-text-4xl lg:tw-text-5xl dark:tw-text-white"
+      >
+        Workspace
+      </h1>
+      <p class="tw-text-sm tw-font-light tw-text-gray-500 dark:tw-text-gray-400">
+        Placeholder
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
