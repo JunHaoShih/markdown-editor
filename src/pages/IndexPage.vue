ea9b85d94b79941aa7dd56472d73364ce0a95ba0
JunHaoShih <victor5517@gmail.com>
2023-11-21 11:51:14 +0800

    refactor: change index page

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,19 +1,24 @@
 <template>
-  <div class="q-pa-sm tw-bg-white tw-text-black dark:tw-bg-darkBg dark:tw-text-stone-200">
-    <q-breadcrumbs class="tw-text-primary-600">
-      <q-breadcrumbs-el icon="home" to="/" />
-    </q-breadcrumbs>
-    <q-separator class="q-mt-sm tw-bg-black dark:tw-bg-stone-200"/>
-    <div class="row justify-center items-center center">
-      <div class="self-center text-h1">{{ 'ヾ(*´∀｀*)ノ' }}</div>
+  <div class="tw-flex tw-flex-col tw-justify-center tw-items-center tw-px-6
+    tw-mx-auto tw-h-screen xl:tw-px-0 dark:tw-bg-stone-800"
+  >
+    <div class="tw-text-center xl:tw-max-w-4xl">
+      <h1 class="tw-mb-3 tw-text-2xl tw-font-bold tw-leading-tight tw-text-gray-900
+        sm:tw-text-4xl lg:tw-text-5xl dark:tw-text-white"
+      >
+        MarkdownEditor
+      </h1>
+      <p class="tw-text-sm tw-font-light tw-text-gray-500 dark:tw-text-gray-400">
+        <router-link to="/workspace"
+          class="tw-font-medium tw-text-primary-600 hover:tw-underline
+          dark:tw-text-primary-500"
+        >
+          {{ $t('indexPage.toWorkspace') }}
+        </router-link>
+      </p>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 </script>
-
-<style lang="sass" scoped>
-.center
-  height: calc( 100vh - 100px )
-</style>
